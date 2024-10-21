1368f4f0da47c3d262f7bb40a5ce442ec925096b
ngaiuane <72505542+ngaiuane@users.noreply.github.com>
2024-10-22 01:10:10 +0200

    Add files via upload

---
+++ application.js
@@ -0,0 +1,8 @@
+function showConfirmation() {
+   document.getElementById('confirmationModal').style.display = 'block';
+    return false;
+}
+
+function confirmSubmission() {
+    window.location.href = 'index.html'; 
+}
